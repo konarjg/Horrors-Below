f08efe45990ce9ea0ffa31a4589d18ece887d722
konarjg <konarskikrzysztof1@gmail.com>
2022-10-21 21:48:35 +0200

    Initial commit

---
+++ Player/Player.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Horrors Below/Player/New Player Stats", fileName = "New Player Stats")]
+public class Player : ScriptableObject
+{
+    
+}
